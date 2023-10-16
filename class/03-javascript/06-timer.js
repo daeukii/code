1c0a9b5791984631b48aba61a7807c08bcc45d41
unknown <gjeodnr2@gmail.com>
2023-10-16 14:19:45 +0900

    231016

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,20 +1,37 @@
+let isStarted = false;
+
 let auth = () => {
-    const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0")
-    document.getElementById("target").innerText = token 
-    document.getElementById("target").style.color = "#" + token
-}
-
-let time = 180
-
-setInterval(function(){
-
-    if(time >=0) {
-        let min = Math.floor( time / 60 )
-        let sec = String(time % 60).padStart(2,"0")
-        document.getElementById("timer").innerText = min + ":" + sec
-        time = time - 1
-    } else{
-        document.getElementById("finish").disabled = true
-    }
     
-},1000)
+    if(isStarted === false){
+        // 타이머가 작동중이 아닐때
+        isStarted = true
+        document.getElementById("finish").disabled = false
+        const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0")
+        document.getElementById("target").innerText = token 
+        document.getElementById("target").style.color = "#" + token
+        
+        
+        let time = 5
+        let timer 
+        
+        timer = setInterval(function(){
+        
+        if(time >=0) {
+            let min = Math.floor( time / 60 )
+            let sec = String(time % 60).padStart(2,"0")
+            document.getElementById("timer").innerText = min + ":" + sec
+            time = time - 1
+        } else{
+            document.getElementById("finish").disabled = true
+            isStarted = false
+            clearInterval(timer)
+        }
+        
+        },1000)
+
+
+    } else { isStarted = true
+        // 타이머가 작동중일때
+        
+    }
+}
